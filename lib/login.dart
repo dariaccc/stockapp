bac33b5264df6799903a4c08d6675c8ffb98010b
dasha <daria.carlberg@gmail.com>
2025-05-16 14:57:00 +0200

    working on login GUI

--- lib/login.dart
+++ lib/login.dart
@@ -12,39 +12,78 @@ class _LoginState extends State<Login> {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Color(0xFF111827),
-        appBar: AppBar(
-          backgroundColor: Color(0xFF111827),
-          title: Text("Login", style: TextStyle(color: Color(0xFFFFFFFF))),
-          centerTitle: true,
-        ),
-        body: SafeArea(
-          child: Center(
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                color: Color(0xFF1F2937),
-              ),
-              height: 400,
-              width: 300,
-              child: Text(
-                "data",
-                style: TextStyle(
-                    color: Color(0xFFFFFFFF),
-                    fontSize: 20,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Image(image: AssetImage('assets/images/logo-test.png')),
+              SizedBox(height: 50),
+              Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Color(0xFF1F2937),
                 ),
-                textAlign: TextAlign.center,
+                height: 400,
+                width: 300,
+                child: Column(
+                  children: [
+                    Text(
+                      "LOGIN",
+                      style: TextStyle(
+                        color: Color(0xFFFFFFFF),
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 50),
+                    RawMaterialButton(
+                      onPressed: () {
+                        Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(builder: (context) => Navigation()),
+                        );
+                      },
+                      fillColor: Color(0xFF111827),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        side: BorderSide(
+                          color: Colors.white, // 👈 your border color
+                          width: 2,
+                        ),
+                      ),
+                      constraints: BoxConstraints.tightFor(
+                        width: 120,
+                        height: 40,
+                      ),
+                      child: Text(
+                          "Login",
+                          style: TextStyle(
+                            color: Color(0xFFFFFFFF),
+                            fontSize: 15,),
+                      ),
+                    ),
+                    Text(
+                        "NO ACCOUNT? REGISTER NOW",
+                        style: TextStyle(color: Color(0x80FFFFFF),
+                        fontSize: 10
+                        ),
+                    ),
+                    SizedBox(height: 10,),
+                    Text(
+                      "Administrator Login",
+                      style: TextStyle(color: Color(0x80FFFFFF),
+                          fontSize: 10
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
+            ],
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.of(context).pushReplacement(
-              MaterialPageRoute(builder: (context) => Navigation()),
-            );
-          },
-          child: Text("click"),
-        ),
+
       ),
     );
   }
