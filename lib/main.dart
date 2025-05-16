1f9e61cf6fb8e5a2f317f035f655753389c5af47
dasha <daria.carlberg@gmail.com>
2025-05-16 10:44:39 +0200

    created the basic menu

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,43 @@ import 'package:flutter/material.dart';
 import '/radar.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Home());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Home extends StatefulWidget {
+  @override
+  State<Home> createState() => _HomeScreenState();
+}
 
+class _HomeScreenState extends State<Home> {
+  int screen_index = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Test home screen"), centerTitle: true),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.of(
-              context,
-            ).push(MaterialPageRoute(builder: (context) => Radar()));
+        appBar: AppBar(title: Text("Home screen"), centerTitle: true),
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (index){
+            setState(() {
+              screen_index = index;
+              /**Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => Screen1()),
+              );**/
+            });
           },
-          child: Text("Radar"),
+          currentIndex: screen_index,
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.indigo,
+          fixedColor: Colors.white,
+          showSelectedLabels: true,
+          showUnselectedLabels: false,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home', ),
+            BottomNavigationBarItem(icon: Icon(Icons.radar), label: 'Radar'),
+            BottomNavigationBarItem(icon: Icon(Icons.upgrade), label: 'Pro'),
+            BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: 'Dashboard'),
+            BottomNavigationBarItem(icon: Icon(Icons.menu), label: 'Menu')
+          ],
         ),
       ),
     );
