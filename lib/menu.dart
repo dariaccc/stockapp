10f855cf735351159fff28ce15ec497a2dddb0d9
dasha <daria.carlberg@gmail.com>
2025-05-28 11:14:25 +0200

    working on the menu

--- lib/menu.dart
+++ lib/menu.dart
@@ -27,37 +27,200 @@ class _MenuState extends State<Menu> {
                 child: Column(
                   children: [
                     Align(
-                    alignment: Alignment.topRight,
-                      child:
-                        MaterialButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: Icon(Icons.close, color: Colors.white),
-                        ),
+                      alignment: Alignment.topRight,
+                      child: MaterialButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Icon(Icons.close, color: Colors.white),
+                      ),
                     ),
                     Container(
-                      width: 320,
-                      height: 55,
+                      width: 300,
+                      height: 40,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10),
                         border: Border.all(color: Color(0xFFFFFFFF)),
                         color: Color(0xFF111827),
                       ),
-                      //stock information
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(vertical: 5),
+                        child: DefaultTextStyle(
+                        style: TextStyle(color: Color(0xFFFFFFFF), fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                        child: Text(
+                          "Home",
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      ),//home
                     ),
 
                     SizedBox(height: 15),
 
                     Container(
-                      width: 320,
-                      height: 55,
+                      width: 300,
+                      height: 40,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10),
                         border: Border.all(color: Color(0xFFFFFFFF)),
                         color: Color(0xFF111827),
                       ),
-                      //stock information
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(vertical: 5),
+                        child: DefaultTextStyle(
+                          style: TextStyle(color: Color(0xFFFFFFFF), fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                          child: Text(
+                            "Radar",
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                      //radar
+                    ),
+
+                    SizedBox(height: 15),
+
+                    Container(
+                      width: 300,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Color(0xFFFFFFFF)),
+                        color: Color(0xFF111827),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(vertical: 5),
+                        child: DefaultTextStyle(
+                          style: TextStyle(color: Color(0xFFFFFFFF), fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                          child: Text(
+                            "Purchase PRO",
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                      //PRO
+                    ),
+
+                    SizedBox(height: 15),
+
+                    Container(
+                      width: 300,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Color(0xFFFFFFFF)),
+                        color: Color(0xFFEF4444), //red background
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(vertical: 5),
+                        child: DefaultTextStyle(
+                          style: TextStyle(color: Color(0xFFFFFFFF), fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                          child: Text(
+                            "Log Out",
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                      //logout
+                    ),
+
+                    SizedBox(height: 15),
+
+                    Container(
+                      width: 300,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Color(0xFFFFFFFF)),
+                        color: Color(0xFFFBBD23), //yellow background
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(vertical: 5),
+                        child: DefaultTextStyle(
+                          style: TextStyle(color: Color(0xFFFFFFFF), fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                          child: Text(
+                            "Dashboard",
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                      //dashboard
+                    ),
+
+                    SizedBox(height: 20),
+
+                    Container(
+                      width: 280,
+                      height: 45,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Color(0xFF111827),
+                      ),
+
+                      //dropdown lang
+                    ),
+
+                    SizedBox(height: 15),
+
+                    Container(
+                      width: 280,
+                      height: 45,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Color(0xFF4F46E5),
+                      ),
+                      //dropdown lang
+                    ),
+
+                    SizedBox(height: 10),
+
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 30),
+                      child: DefaultTextStyle(
+                        style: TextStyle(color: Color(0xFFFFFFFF), fontSize: 8),
+                        child: Text(
+                          "VANTYX is not a registered broker-dealer or "
+                          "investment advisor. Trading involves risk and may "
+                          "result in financial loss. Market data is provided for "
+                          "informational purposes only and is not intended for trading"
+                          " or investment advice. Past performance does not guarantee "
+                          "future results. Always do your own research before making "
+                          "financial decisions..",
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+
+                    //SizedBox(height: 10),
+
+                    //Spacer(), // pushes content above upwards
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20.0,
+                        vertical: 10,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Image(
+                            height: 35,
+                            image: AssetImage('assets/images/logo-test.png'),
+                          ),
+                          Container(
+                            width: 70,
+                            height: 30,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30),
+                              color: Color(0xFFFFFFFF),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
